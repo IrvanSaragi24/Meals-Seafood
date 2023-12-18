556f6b74d16f1236fb4c615f3620891a54537cbf
irvan <pradaindo24@gmail.com>
2023-12-18 09:58:17 +0700

    Initial Commit

---
+++ Meals_Seafood/Meals_SeafoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meals_SeafoodApp.swift
+//  Meals_Seafood
+//
+//  Created by Irvan P. Saragi on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Meals_SeafoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
